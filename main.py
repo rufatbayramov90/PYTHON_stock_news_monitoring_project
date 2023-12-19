d1225382c5b90f311568fd1a3923609506f06f3d
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-12-19 11:02:50 +0400

    use the News API to get articles related to the COMPANY_NAME

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "P7FEEGDXXECOBSTM"
+NEWS_API_KEY = "1c5a322ed48b46e39bae5f45f649dffb"
 
 stock_params = {
     "function" : "TIME_SERIES_DAILY",
@@ -30,3 +31,13 @@ print(difference)
 
 diff_percent = (difference / float(yesterday_closing_price)) * 100
 print(diff_percent)
+
+if diff_percent > 1:
+    news_params = {
+        "apikey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+    print(articles)
